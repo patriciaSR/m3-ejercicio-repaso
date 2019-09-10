8f8ba2ef30118dff0f78c823e683652f475313f9
patriciaSR <patricia.suarez.psr@gmail.com>
2019-09-10 18:45:10 +0200

    add first JSX structure to print 1 element of clubs array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.scss';
+import {clubs} from './clubs';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      clubs: clubs
+    }
+  }
 
   render() {
     return (
       <div className="app">
-      :)
+      <ul className="clubs__list">
+        <li className="list__club">
+          <div className="club__icon">
+            <i className={this.state.clubs[0].fa}></i>
+          </div>
+          <h2 className="club__name">{this.state.clubs[0].name}</h2>
+          <h3 className="club__members">Members</h3>
+          <ul className="members__list">
+            <li className="list__member">{this.state.clubs[0].members[0]}</li>
+          </ul>
+        </li>
+      </ul>
       </div>
     );
   }
