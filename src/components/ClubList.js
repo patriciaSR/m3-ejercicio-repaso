f4ceac4cd37bf3d486ac15dcad98e69e5ac12dc3
patriciaSR <patricia.suarez.psr@gmail.com>
2019-09-10 19:53:07 +0200

    add docs folder

--- src/components/ClubList.js
+++ src/components/ClubList.js
@@ -4,23 +4,23 @@ import PropTypes from 'prop-types';
 import './ClubList.scss';
 
 class ClubList extends React.Component {
-render(){
-  return(
-    <ul className="clubs__list">
-          {this.props.clubs.map(club => (
-              <Club 
-              key={club.id}
-              id={club.id}
-              name={club.name}
-              fa={club.fa}
-              members={club.members}
-              />
-            )
-          )}
+  render() {
+    return (
+      <ul className="clubs__list">
+        {this.props.clubs.map(club => (
+          <Club
+            key={club.id}
+            id={club.id}
+            name={club.name}
+            fa={club.fa}
+            members={club.members}
+          />
+        )
+        )}
 
-        </ul>
-  )
-}
+      </ul>
+    )
+  }
 }
 
 ClubList.propTypes = {
