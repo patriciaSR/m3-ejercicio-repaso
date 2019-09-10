c4bf23eda25df88a85bb988510bb57ad619847c7
patriciaSR <patricia.suarez.psr@gmail.com>
2019-09-10 20:17:44 +0200

    merge filterbranch and update docs folder

---
+++ docs/precache-manifest.4bde896d3f2cb11868c28968e14fd869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bd33453cc690eee5d449a8ecd389f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a00920bf74e74dab8cd",
+    "url": "./static/css/main.8aa8096c.chunk.css"
+  },
+  {
+    "revision": "45b8009b51331235a1cc",
+    "url": "./static/js/2.cdee14e9.chunk.js"
+  },
+  {
+    "revision": "4a00920bf74e74dab8cd",
+    "url": "./static/js/main.2cda5481.chunk.js"
+  },
+  {
+    "revision": "2dfd61ed9a0ae2d6df2d",
+    "url": "./static/js/runtime~main.144a0f7c.js"
+  }
+]);
